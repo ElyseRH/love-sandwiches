caa3c41bc41489b78085e0a9a2be501480639643
ElyseRH <elyserhoran@gmail.com>
2021-09-15 16:55:00 +0000

    Add conversion and check for user input

--- run.py
+++ run.py
@@ -36,6 +36,7 @@ def validate_data(values):
     Raises ValueError if cannot be converted or if there aren't exactly 6 values.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(f"Exactly 6 values required, you provided {len(values)}")
     except ValueError as e:
